676f281d2143e98df4073a592094a41bb4b7cdf9
h3lax <axel.poidebard@gmail.com>
2025-03-16 18:05:39 +0000

    16 mars update with model training v.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dev-container-python-template
+
+# Ressources
+[skeatlearn Doc]: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+[Model Selection]: https://ploomber.io/blog/nested-cv/
--- src/Scraps/scrapPollution_global.py
+++ src/Scraps/scrapPollution_global.py
@@ -9,11 +9,13 @@ if __name__ == '__main__':
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.3'
     }
 
-    csv_file = '../data/bronze/Pollution_urls.csv'
+    csv_file = '../../data/bronze/Pollution_urls.csv'
     df_urls = pd.read_csv(csv_file)
+    # df_urls = ['https://www.numbeo.com/pollution/country_result.jsp?country=Cambodia']
     data = []
 
     for url in df_urls['url']:
+    # for url in df_urls:
         try:
             response = requests.get(url, headers=headers, timeout=10)
             response.raise_for_status()  # Raise an error for bad responses
@@ -23,21 +25,25 @@ if __name__ == '__main__':
             h1_text = soup.h1.text if soup.h1 else ""
             match = re.search(r'in\s([A-Za-z\s]+)', h1_text)
             pays = match.group(1).strip().replace(' ', '_') if match else "Unknown"
-
+            print(f"Processing {pays}...")
             # Récupération des données de la table
-            table = soup.select_one('table.table_builder_with_value_explanation.data_wide_table')
+            table = soup.select_one('table.table_indices')
             if table:
                 rows = table.select('tr')
-                for row in rows:
-                    cells = row.find_all('td')
-                    if len(cells) > 2:
-                        data.append([pays, cells[0].text.strip(), cells[2].text.strip(), cells[3].text.strip()])
+                indice = []
+                for x in [1, 2]:
+                    cells = rows[x].find_all('td')
+                    indice.append(cells[1].text.strip())
+                if len(indice) > 1:
+                    data.append([pays, indice[0], indice[1]])
         except Exception as e:
             print(f"Error processing URL {url}: {e}")
 
+    
     # Sauvegarde des données dans un fichier CSV
-    dir_path = '../data/bronze'
+    dir_path = '../../data/bronze'
     os.makedirs(dir_path, exist_ok=True)
-    res = pd.DataFrame(data, columns=['Country', 'Pollution_Label', 'Pollution_Index', 'Pollution_Qualifier'])
-    res.to_csv(os.path.join(dir_path, 'tentative.csv'), index=False)
+
+    res = pd.DataFrame(data, columns=['Country', 'Pollution_index','Pollution_index_Exp'])
+    res.to_csv(os.path.join(dir_path, 'Pollution_global.csv'), index=False)
     print("Fichier CSV sauvegardé.")
